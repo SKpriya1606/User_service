b1b09642cea402b833a0cb6e222e488e27c86820
SKpriya1606 <83779316+SKpriya1606@users.noreply.github.com>
2021-05-08 01:24:49 +0530

    Add files via upload

---
+++ user.service/src/main/java/com/cts/training/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.cts.training.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cts.training.bean.User;
+
+public interface UserRepository extends JpaRepository<User, Long>{
+	
+}
+
---
+++ user.service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into user(id,username,password,address,phone) values(1001L,'mahesh','mahesh123','Bangalore',9874635246L);
+insert into user(id,username,password,address,phone) values(1002L,'rahesh','rahesh1223','chennai',9874635244L);
+insert into user(id,username,password,address,phone) values(1003L,'sahesh','sahesh123','mumbai',9874635566L);
+insert into user(id,username,password,address,phone) values(1004L,'suresh','suresh123','pune',9874635231L);
+insert into user(id,username,password,address,phone) values(1005L,'mahi','mah123','Hydrabad',9874635687L);
